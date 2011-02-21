8bc4b6c84cc01bf41b6ccdc92ea75e95b9b0708c
thierry <thierry.henrio@gmail.com>
2011-02-21 19:42:50 +0100

    Refactor to extract code

--- lib/tinisculus.rb
+++ lib/tinisculus.rb
@@ -1,3 +1,21 @@
-module Tinisculus
-  # Your code goes here...
+
+class Wheel
+  def self.charset
+    [
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+      ".", ",", "?", "!", "'", "\"", " "
+    ]
+  end
+  def self.encode( message, position )
+    message.chars.collect do |char|
+      self.charset[(self.findindex(char) + position) % self.charset.count]
+    end.join
+  end
+  def self.findindex(char)
+    self.charset.index(char)
+  end
 end
--- spec/tinisculus_spec.rb
+++ spec/tinisculus_spec.rb
@@ -1,27 +1,6 @@
 require 'spec_helper'
 require "tinisculus"
 
-class Wheel
-  def self.charset
-    [
-      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-      ".", ",", "?", "!", "'", "\"", " "
-    ]
-  end
-  def self.encode( message, position )
-    message.chars.collect do |char|
-      self.charset[(self.findindex(char) + position) % self.charset.count]
-    end.join
-  end
-  def self.findindex(char)
-    self.charset.index(char)
-  end
-end
-
 describe Wheel do
   describe "charset" do
     it 'is as indicated in spec ...' do
