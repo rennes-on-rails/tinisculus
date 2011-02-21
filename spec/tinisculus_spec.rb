b4688aa9c279a59f37f4d97b4512422c17ff6978
thierry <thierry.henrio@gmail.com>
2011-02-21 18:57:16 +0100

    desc tinisculus charset

--- spec/tinisculus_spec.rb
+++ spec/tinisculus_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
+require "tinisculus"
 
-describe '' do
-  it "fails" do
-    true.should == true
+class Wheel
+  def self.charset
+    [
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+      "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+      ".", ",", "?", "!", "'", "\"", " "
+    ]
+  end
+end
+
+describe Wheel do
+  describe "charset" do
+    it 'is as indicated in spec ...' do
+      Wheel.charset.should == [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+        ".", ",", "?", "!", "'", "\"", " "
+      ]
+    end
   end
 end
