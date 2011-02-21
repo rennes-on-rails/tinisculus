d03f7bd6151647f6f6c4a6d236a5655840a883bf
thierry <thierry.henrio@gmail.com>
2011-02-21 19:23:57 +0100

    Encode is working

--- spec/tinisculus_spec.rb
+++ spec/tinisculus_spec.rb
@@ -13,7 +13,8 @@ class Wheel
     ]
   end
   def self.encode( char, position )
-    'f'
+    charpos = self.findindex(char)
+    self.charset[charpos + position]
   end
   def self.findindex(char)
     self.charset.index(char)
@@ -35,9 +36,9 @@ describe Wheel do
     it 'should have a method encode which returns "f" when passed "a" as character and 5 as position' do
       Wheel.encode( 'a', 5 ).should == 'f'
     end
-    #it 'should have a method encode which returns "h" when passed "c" as character and 5 as position' do
-    #  Wheel.encode( 'c', 5 ).should == 'h'
-    #end
+    it 'should have a method encode which returns "h" when passed "c" as character and 5 as position' do
+      Wheel.encode( 'c', 5 ).should == 'h'
+    end
     it 'should have a method findindex which returns 0 when pass "0"' do
       Wheel.findindex( "0").should == 0
     end
